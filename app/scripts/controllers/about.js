b779f61dad9e776012184d3b01dc9ec0646aa96a
David Da Silva <contact@daviddasilva.net>
2016-02-04 10:30:42 +0100

    Yo scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crudAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the crudAngularApp
+ */
+angular.module('crudAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crudAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the crudAngularApp
+ */
+angular.module('crudAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
